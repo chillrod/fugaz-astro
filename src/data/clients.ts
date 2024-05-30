3035672dd6e86d3c84f6d20c3300a5ba7deeeed6
chillrod <chillrod77@gmail.com>
2024-05-30 16:59:19 -0300

    feat: clients area, changes in UI

---
+++ src/data/clients.ts
@@ -0,0 +1,32 @@
+export const clients = [
+  {
+    name: "Kairū",
+    route: "/kairu",
+    url: "https://www.instagram.com/kairurest/",
+  },
+  {
+    name: "Balthazar",
+    route: "/balthazar",
+    url: "https://www.instagram.com/balthazarvix/",
+  },
+  {
+    name: "Kairū",
+    route: "/kairu",
+    url: "https://www.instagram.com/kairurest/",
+  },
+  {
+    name: "Balthazar",
+    route: "/balthazar",
+    url: "https://www.instagram.com/balthazarvix/",
+  },
+  {
+    name: "Kairū",
+    route: "/kairu",
+    url: "https://www.instagram.com/kairurest/",
+  },
+  {
+    name: "Balthazar",
+    route: "/balthazar",
+    url: "https://www.instagram.com/balthazarvix/",
+  },
+];
