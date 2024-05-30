305228901b1785fc04751f86309105c2c7631488
chillrod <chillrod77@gmail.com>
2024-05-30 17:47:54 -0300

    fix: sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService} from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
   integrations: [
     tailwind(),
     icon({
