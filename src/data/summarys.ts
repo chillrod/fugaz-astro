5c3547b309c78412e2e7c4cfd2760b68d74a3384
chillrod <chillrod77@gmail.com>
2024-06-02 12:43:43 -0300

    feat: service page, changes in menu, started about page

--- src/data/summarys.ts
+++ src/data/summarys.ts
@@ -11,4 +11,12 @@ export const summarys = [
     title: "Gestão Administrativa",
     text: "Potencialize sua gestão. Consultoria em administração para otimizar processos, reduzir custos e maximizar eficiência. Alcance resultados sólidos. Consulte-nos para transformar desafios em oportunidades de crescimento.",
   },
+  {
+    title: "Gestão de Pessoas",
+    text: "Desenvolva sua equipe. Consultoria especializada em gestão de pessoas. Capacite seus colaboradores, aprimore a comunicação e fortaleça o trabalho em equipe. Transforme seu negócio com talentos engajados e motivados. Consulte-nos!",
+  },
+  {
+    title: "Marketing e Comunicação",
+    text: "Destaque-se no mercado. Consultoria em marketing e comunicação para fortalecer sua marca e atrair mais clientes. Estratégias personalizadas para alavancar seu negócio. Consulte-nos para transformar desafios em oportunidades de crescimento.",
+  },
 ];
