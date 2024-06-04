e6f9caf017ff383ed602a1c70d6899cf46371b9d
chillrod <chillrod77@gmail.com>
2024-06-04 00:53:28 -0300

    fix: layout changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,3 @@
-import defaultTheme from "tailwindcss/defaultTheme";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -7,6 +5,21 @@ export default {
     extend: {
       fontFamily: {
         serif: ["Noto Serif Khojki Variable"],
+      },
+      backgroundImage: {
+        star: "url('/star.svg')",
+        grid: `linear-gradient(to right, rgb(254 215 170) 1px, transparent 1px),
+        linear-gradient(to bottom, rgb(254 215 170) 1px, transparent 1px)`,
+        gridRotated: `linear-gradient(to top, rgb(254 215 170) 2px, transparent 1px),
+        linear-gradient(to right, rgb(254 215 170) 1px, transparent 1px)`,
+      },
+      backgroundSize: {
+        "star-md": "15%",
+        "star-sm": "35%",
+        "grid-size": "40px 40px",
+      },
+      backgroundPosition: {
+        "posi-star": "100% 0%",
       },
     },
   },
